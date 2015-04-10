82bc3091b75a8494f9f257c11328fc3bbaa8a2ca
Lucas Miranda Dourado <lucasmirandadourado@gmail.com>
2015-04-10 14:43:29 -0300

    Teste
    Feito o Teste um pouco mais ou menos!

--- src/com/uepb/teste/MatrizTest.java
+++ src/com/uepb/teste/MatrizTest.java
@@ -1,5 +1,6 @@
 package com.uepb.teste;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.uepb.algoritmo.Matriz;
@@ -9,11 +10,11 @@ public class MatrizTest {
 	Matriz matriz;
 
 	@Test
-	public void multiplicar() {
+	public void multiplicar() throws Exception {
 		matriz = new Matriz();
-		int[][] matrizA = {{1, 2, 3},{},{}};
-		int[][] matrizB = {{} , {} , {} };
-		
-		
+		int[][] matrizA = {{1, 2, 3},{4, 5, 6},{1, 1, 1}};
+		int[][] matrizB = {{1, 2, 3}, {1, 2, 3} , {1, 2, 3}};
+		int[][] result = {{6, 12, 18}, {15, 30, 45}, {3, 6, 9}};
+		Assert.assertEquals(result, matriz.multiplicaMatrizes(matrizA, matrizB));		
 	}
 }
