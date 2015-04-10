832155eb8db6033e45c64e2454ea78c4cf6fd05d
Lucas Miranda Dourado <lucasmirandadourado@gmail.com>
2015-04-10 14:17:41 -0300

    Pacote de test
    Criação da class de Test.
    Test de Matriz

---
+++ src/com/uepb/teste/MatrizTest.java
@@ -0,0 +1,19 @@
+package com.uepb.teste;
+
+import org.junit.Test;
+
+import com.uepb.algoritmo.Matriz;
+
+public class MatrizTest {
+
+	Matriz matriz;
+
+	@Test
+	public void multiplicar() {
+		matriz = new Matriz();
+		int[][] matrizA = {{1, 2, 3},{},{}};
+		int[][] matrizB = {{} , {} , {} };
+		
+		
+	}
+}
