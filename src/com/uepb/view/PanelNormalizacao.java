71a590622c69f3564329480812d153cab5559291
Daniel Marques <danmarquesvg@gmail.com>
2015-04-10 16:11:53 -0300

    Interface Gráfica
    Tela de Normalização quase completada, falta corrigir as funções de
    normalização

--- src/com/uepb/view/PanelNormalizacao.java
+++ src/com/uepb/view/PanelNormalizacao.java
@@ -4,10 +4,14 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelNormalizacao extends JPanel {
 
 	PanelPlanoCartesiano panelPlanoCartesiano = new PanelPlanoCartesiano();
+	static JLabel labelDCX;
+	static JLabel labelDCY;
 	
 	/**
 	 * Create the panel.
@@ -17,6 +21,14 @@ public class PanelNormalizacao extends JPanel {
 		
 		setBounds(0, 0, 1008, 660);
 		setLayout(null);
+		panelPlanoCartesiano.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent eventoDeMouse) {
+				labelDCX.setText("DCX (X): "+Integer.toString(eventoDeMouse.getX() - 300));
+				labelDCY.setText("DCY (Y): "+Integer.toString((eventoDeMouse.getY() - 300) * -1));
+				repaint();
+			}
+		});
 		panelPlanoCartesiano.setBounds(400, 30, 600, 600);
 		
 		add(panelPlanoCartesiano);
@@ -32,6 +44,18 @@ public class PanelNormalizacao extends JPanel {
 		labelCoordenadasDoDispositivo.setFont(new Font("Century Gothic", Font.BOLD, 20));
 		labelCoordenadasDoDispositivo.setBounds(10, 0, 307, 41);
 		panelBackgroundCoordenadasDoDispositivo.add(labelCoordenadasDoDispositivo);
+		
+		labelDCX = new JLabel("DCX (X): ");
+		labelDCX.setForeground(Color.WHITE);
+		labelDCX.setFont(new Font("Century Gothic", Font.PLAIN, 18));
+		labelDCX.setBounds(10, 82, 348, 41);
+		add(labelDCX);
+		
+		labelDCY = new JLabel("DCY (Y): ");
+		labelDCY.setForeground(Color.WHITE);
+		labelDCY.setFont(new Font("Century Gothic", Font.PLAIN, 18));
+		labelDCY.setBounds(10, 113, 348, 41);
+		add(labelDCY);
 		
 
 	}
